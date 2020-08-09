dfdfd30885ef4d8d2326ff1ecc692d9562089e64
cbxa <895001956@qq.com>
2020-08-09 12:22:36 +0800

    希尔排序

---
+++ sort/ShellSort.java
@@ -0,0 +1,58 @@
+package com.cbx.sort;
+
+import java.util.Arrays;
+
+/**
+ * 简单插入排序的高效版本
+ * 交换法
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+         int[] arr={8,9,1,7,2,3,5,4,6,0};
+//         shellSort(arr);
+         shellSort2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    /**
+     * 交换法
+     * @param arr
+     */
+    public static void shellSort(int[] arr){
+        int temp=0;
+        for(int gap=arr.length/2;gap>0;gap/=2){
+            //共gap组，步长gap
+            for(int i=gap;i<arr.length;i++){
+                for (int j=i-gap;j>=0;j-=gap){
+                    if (arr[j]>arr[j+gap]){
+                        temp=arr[j];
+                        arr[j]=arr[j+gap];
+                        arr[j+gap]=temp;
+                    }
+                }
+            }
+            System.out.println("步长"+gap+ Arrays.toString(arr));
+        }
+    }
+
+    /**
+     * 移位法（是进一步的优化）
+     * @param arr
+     */
+    public static void shellSort2(int[] arr){
+        for(int gap=arr.length/2;gap>0;gap/=2) {
+             for(int i=gap;i<arr.length;i++){
+                 int j=i;
+                 int temp=arr[j];
+                 if (arr[j]<arr[j-gap]){
+                     while(j-gap>=0&&temp<arr[j-gap]){
+                        arr[j]=arr[j-gap];
+                        j-=gap;
+                     }
+                     arr[j]=temp;
+                 }
+             }
+        }
+    }
+}
